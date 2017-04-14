80667988f2ac1a6ca3360ba388819e582cce6a79
YangJiaYing <1570263943@qq.com>
2017-04-14 19:17:13 +0800

    add

--- app/src/main/java/com/example/materialtest/Activity/MainActivity.java
+++ app/src/main/java/com/example/materialtest/Activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.materialtest.Activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
